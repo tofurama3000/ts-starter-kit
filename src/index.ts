3c3525721977457b885e0788f7c6f213941e8314
tofurama3000 <tofurama3000@users.noreply.github.com>
2018-12-18 07:36:30 -0700

    Updated src files

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,3 @@
-import Koa from 'koa';
-import mongoose from 'mongoose';
-import { hello } from './hello';
+import { hello } from "./hello";
 
-mongoose.connect('mongodb://localhost/prototype');
-
-const app = new Koa();
-const port: number = 3000;
-
-app.use(async (ctx: Koa.Context) => {
-  ctx.body = hello();
-});
-
-app.listen(port);
+hello();
